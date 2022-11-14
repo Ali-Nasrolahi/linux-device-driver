773c558528995b6c76d25612b52bfd07dea22d2e
Ali Nasrolahi <A.Nasrolahi01@gmail.com>
2022-11-14 11:56:43 +0330

    name module param added.

--- scull/main.c
+++ scull/main.c
@@ -5,18 +5,28 @@
 #include <linux/sched.h>
 #include <linux/kernel.h>
 
+int scull_major = SCULL_MAJOR;
+int scull_minor = 0;
+int scull_nr_devs = SCULL_NR_DEVS; /* number of bare scull devices */
+static char *name = "World";
+
+module_param(scull_major, int, S_IRUGO);
+module_param(scull_minor, int, S_IRUGO);
+module_param(scull_nr_devs, int, S_IRUGO);
+module_param(name, charp, S_IRUGO);
+
 MODULE_AUTHOR("Ali Nasrolahi");
 MODULE_LICENSE("GPL");
 
 int __init scull_init(void)
 {
-	PDEBUG("Hello World!!");
+	PDEBUG("Hello %s!!", name);
 	return 0;
 }
 
 void __exit scull_cleanup(void)
 {
-	PDEBUG("Have a nice one, bye!!");
+	PDEBUG("Have a nice one, bye %s!!", name);
 }
 
 module_init(scull_init);
